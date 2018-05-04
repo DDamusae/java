2891f59d51f3105168a3ecd53888e286eb5acd24
DDamusae <satan62236@gmail.com>
2018-05-04 16:29:09 +0900

    단어의 개수

---
+++ 단어의 개수/src/main/Main.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine().trim();
+		int count = 0;
+		
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == ' ' && input.charAt(i + 1) != ' ') {
+				count++;
+			}
+
+		}
+		boolean empty = true;
+		for(int i=0; i<input.length(); i++) {
+			if(input.charAt(i) == ' ' && input.charAt(i+1) == ' ') {
+				empty = true;
+			}else {
+				empty = false;
+			}
+			
+		}
+		
+		if(empty) {
+			System.out.println(0);
+		}else {
+			System.out.println(count+1);
+		}
+	}
+}
