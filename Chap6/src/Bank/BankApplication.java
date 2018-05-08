b55657584eb2fbadfb7474a88dbf2a58b85ab1c1
DDamusae <satan62236@gmail.com>
2018-05-08 14:15:36 +0900

    chap6 확인문제

---
+++ Chap6/src/Bank/BankApplication.java
@@ -0,0 +1,108 @@
+package Bank;
+
+import java.util.Scanner;
+
+public class BankApplication {
+	private static Account[] accountArray = new Account[100];
+	private static Scanner scanner = new Scanner(System.in);
+	static int i = 0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean run = true;
+
+		while (run) {
+			System.out.println("--------------------------------------");
+			System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
+			System.out.println("--------------------------------------");
+			System.out.print("선택> ");
+
+			int selectNo = scanner.nextInt();
+
+			if (selectNo == 1) {
+				createAccount();
+			} else if (selectNo == 2) {
+				accountList();
+			} else if (selectNo == 3) {
+				deposit();
+			} else if (selectNo == 4) {
+				withdraw();
+			} else if (selectNo == 5) {
+				run = false;
+			}
+		}
+		System.out.println("프로그램 종료");
+
+	}
+
+	private static void createAccount() {
+		// TODO Auto-generated method stub
+
+		accountArray[i] = new Account(null, null, 0);
+
+		System.out.println("-----------------");
+		System.out.println("계좌 생성");
+		System.out.println("-----------------");
+
+		System.out.print("계좌번호: ");
+		accountArray[i].setAno(scanner.next());
+
+		System.out.print("계좌주: ");
+		accountArray[i].setOwner(scanner.next());
+
+		System.out.print("초기입금액: ");
+		accountArray[i].setBalance(scanner.nextInt());
+
+		System.out.println("계좌가 생성되었습니다.");
+		i++;
+	}
+
+	private static void accountList() {
+		// TODO Auto-generated method stub
+		System.out.println("-----------------");
+		System.out.println("계좌 목록");
+		System.out.println("-----------------");
+		for (int i = 0; accountArray[i] != null; i++) {
+			System.out.print(accountArray[i].getAno() + "  " + accountArray[i].getOwner() + "  " + accountArray[i].getBalance());
+			System.out.println();
+		}
+	}
+
+	private static void deposit() {
+	 // TODO Auto-generated method stub
+		
+		System.out.println("-----------------");
+		System.out.println("예끔");
+		System.out.println("-----------------");
+		System.out.print("계좌번호:");
+		Account ac = findAccount(scanner.next());
+		System.out.print("예금액: ");
+		ac.setBalance(ac.getBalance()+scanner.nextInt());
+		
+		System.out.println("예금이 성공되었습니다.");
+		System.out.println();
+	 }
+
+	private static void withdraw() {
+		// TODO Auto-generated method stub
+		System.out.println("-----------------");
+		System.out.println("출금");
+		System.out.println("-----------------");
+		System.out.print("계좌번호:");
+		Account ac = findAccount(scanner.next());
+		System.out.print("출금액: ");
+		ac.setBalance(ac.getBalance()-scanner.nextInt());
+		
+		System.out.println("출금이 성공되었습니다.");
+		System.out.println();
+	}
+
+	private static Account findAccount(String ano) {
+		for(int i=0; i<accountArray.length; i++) {
+			if(accountArray[i].getAno().equals(ano)) {
+				return accountArray[i];
+			}
+		}
+		return null;
+	 }
+}
---
+++ Chap6/src/printer/Printer.java
@@ -0,0 +1,33 @@
+package printer;
+
+public class Printer {
+	Printer(){}
+
+	//16번
+//	void println(int n){
+//		System.out.println(n);
+//	}
+//	void println(boolean x) {
+//		System.out.println(x);
+//	}
+//	void println(double n) {
+//		System.out.println(n);
+//	}
+//	void println(String str) {
+//		System.out.println(str);
+//	}
+	
+	//17번
+	static void println(int n){
+		System.out.println(n);
+	}
+	static void println(boolean x) {
+		System.out.println(x);
+	}
+	static void println(double n) {
+		System.out.println(n);
+	}
+	static void println(String str) {
+		System.out.println(str);
+	}
+}
